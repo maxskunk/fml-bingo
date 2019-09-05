b10497f45702c47db1a904e5eafc26d59a0bef62
Brian J Weis <brian@brianweis.net>
2019-09-04 19:25:01 -0600

    Added  Bingo Board

---
+++ web-app/src/app/bingo-board/bingo-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bingo-board',
+  templateUrl: './bingo-board.component.html',
+  styleUrls: ['./bingo-board.component.scss']
+})
+export class BingoBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
